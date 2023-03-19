9a7bb5cc914f03cf7515e684611108a126d851da
seo-faper <dswhdcks@gmail.com>
2023-03-19 18:27:14 +0900

    백준 코딩은 체육과목입니다.

---
+++ ProblemSolve/25314.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    for (int i = 0; i < n / 4; i++)
+    {
+        cout << "long ";
+    }
+    cout << "int" << endl;
+    return 0;
+}
