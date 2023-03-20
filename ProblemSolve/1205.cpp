8480d683d04f5bdaae9ce8d433a9b51887ebfa22
seo-faper <dswhdcks@gmail.com>
2023-03-20 14:25:58 +0900

    백준 2167

--- ProblemSolve/1205.cpp
+++ ProblemSolve/1205.cpp
@@ -9,27 +9,6 @@ int main()
     cin >> N >> S >> P;
     int ans = 0;
     int arr[P];
-    vector<int> v;
-    for (int i = 0; i < N; i++)
-    {
-        cin >> arr[i];
-        v.push_back(arr[i]);
-    }
-    for (int i = 0; i < P; i++)
-    {
-        if (S >= arr[i])
-        {
-            v.insert(v.begin() + i, S);
-            if (S == arr[i] && v.size() > P)
-            {
-                cout << -1 << endl;
-                break;
-            }
-            else
-                cout << i + 1 << endl;
-            break;
-        }
-    }
 
     return 0;
 }
