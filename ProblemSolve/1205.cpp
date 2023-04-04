fc23150cc4701c996e98ebc34156a737fc37de0e
seo-faper <dswhdcks@gmail.com>
2023-04-04 19:34:29 +0900

    백준 등수 구하기

--- ProblemSolve/1205.cpp
+++ ProblemSolve/1205.cpp
@@ -3,12 +3,48 @@
 #include <algorithm>
 using namespace std;
 int N, S, P;
-
+int arr[51];
 int main()
 {
     cin >> N >> S >> P;
-    int ans = 0;
-    int arr[P];
+    int ans = -1;
+    for (int i = 0; i < P; i++)
+    {
+        if (i < N)
+            cin >> arr[i];
+        else
+            arr[i] = -1;
+    }
+    arr[P] = S;
+    bool gate = false;
+    for (int i = P; i >= 0; i--)
+    {
+        if (i == P && arr[i] > arr[i - 1])
+        {
+            int tmp = arr[i - 1];
+            arr[i - 1] = arr[i];
+            arr[i] = tmp;
+            ans = i;
+            gate = true;
+            //    cout << "랭킹 진입 성공 " << ans << endl;
+        }
+        if (!gate)
+        {
+            //   cout << "랭킹 진입 실패" << endl;
+            break;
+        }
+        // cout << arr[i] << ", " << S << ", " << i << endl;
+        if (arr[i] <= S)
+        {
+            ans = i;
+        }
+    }
+    // for (int i = 0; i <= P; i++)
+    //     cout << arr[i] << " ";
+    // cout << endl;
+    if (ans != -1)
+        ans++;
+    cout << (ans) << endl;
 
     return 0;
 }
