726e248c84819b00a67bf8c62800b4f8804214ce
seo-faper <dswhdcks@gmail.com>
2023-04-15 02:09:43 +0900

    백준 11557

---
+++ ProblemSolve/11557.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int T, N;
+int main()
+{
+    cin >> T;
+    while (T--)
+    {
+        cin >> N;
+        int m = 0;
+        string ans = "";
+        while (N--)
+        {
+            string k;
+            int l;
+            cin >> k >> l;
+            if (l >= m)
+            {
+                m = l;
+                ans = k;
+            }
+        }
+        cout << ans << endl;
+    }
+    return 0;
+}
