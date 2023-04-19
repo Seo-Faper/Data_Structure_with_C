3d30836304439e187d2ebbbffbbb7aa0e13fdc56
seo-faper <dswhdcks@gmail.com>
2023-04-19 19:16:24 +0900

    백준 니모를 찾아서

--- ProblemSolve/10173.cpp
+++ ProblemSolve/10173.cpp
@@ -5,14 +5,13 @@ int main()
     string s;
     while (1)
     {
-        cin >> s;
+        getline(cin, s);
         if (s == "EOI")
             break;
         for (int i = 0; i < s.size(); i++)
         {
             s[i] = toupper(s[i]);
         }
-
         if (s.find("NEMO") != string::npos)
         {
             cout << "Found" << endl;
