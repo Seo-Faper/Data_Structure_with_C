3a8dda05b687f1a83582fdf33a821009c09d250e
seo-faper <dswhdcks@gmail.com>
2023-04-20 13:29:02 +0900

    당근!

--- ProblemSolve/t.cpp
+++ ProblemSolve/t.cpp
@@ -4,15 +4,29 @@
 #include <string>
 using namespace std;
 
+int f(int n)
+{
+    if (n < 1)
+        return 2;
+    else
+        return 4 * f(n - 1) + 1;
+}
+int a[4][10];
 int main()
 {
-    string s;
-    stack<string> v;
-    cin >> s;
-    for (int i = s.size() - 1; i >= 0; i--)
-    {
-        cout << s[i] << endl;
-    }
 
+    int cnt = 0;
+    for (int i = 0; i < 4; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+
+            if (i == 2 && j == 8)
+            {
+                cout << cnt << endl;
+            }
+            cnt++;
+        }
+    }
     return 0;
 }
