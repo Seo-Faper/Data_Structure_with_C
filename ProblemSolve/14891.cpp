1b974fd5227a01cfbae5837bd864b59ae5536a9f
seo-faper <dswhdcks@gmail.com>
2023-05-04 02:38:20 +0900

    백준 톱니바퀴

--- ProblemSolve/14891.cpp
+++ ProblemSolve/14891.cpp
@@ -4,8 +4,23 @@
 #include <cmath>
 using namespace std;
 
+int dir[4];
 string s[4];
 int k, n, d;
+void setDir(int i, int D)
+{
+    int next = i + 1;
+    int prev = i - 1;
+    dir[i] = D;
+    if (next < 4 && dir[next] == 0 && s[i][2] != s[next][6])
+    {
+        setDir(next, D * -1);
+    }
+    if (prev >= 0 && dir[prev] == 0 && s[i][6] != s[prev][2])
+    {
+        setDir(prev, D * -1);
+    }
+}
 string toString(const deque<char> *q)
 {
     string tmp = "";
@@ -51,95 +66,18 @@ int main()
     for (int i = 0; i < k; i++)
     {
         cin >> n >> d;
-        n--;
-
-        q[n] = rotate(d, q[n]);
-        s[n] = toString(&q[n]);
-        if (n == 0)
+        n -= 1;
+        setDir(n, d);
+        for (int i = 0; i < 4; i++)
         {
-            if (s[0][2] != s[1][6]) // 2 :오른쪽 6 : 왼쪽
+            if (dir[i] != 0)
             {
-                d *= -1;
-                q[1] = rotate(d, q[1]);
-                s[1] = toString(&q[1]);
-                if (s[1][2] != s[2][6])
-                {
-                    d *= -1;
-                    q[2] = rotate(d, q[2]);
-                    s[2] = toString(&q[2]);
-                    if (s[2][2] != s[3][6])
-                    {
-                        d *= -1;
-                        q[3] = rotate(d, q[3]);
-                        s[3] = toString(&q[3]);
-                    }
-                }
+                q[i] = rotate(dir[i], q[i]);
+                s[i] = toString(&q[i]);
             }
         }
-        else if (n == 1) // 1번을 회전했을 때
-        {
-
-            if (s[1][2] != s[2][6])
-            {
-                d *= -1;
-                q[2] = rotate(d, q[2]);
-                s[2] = toString(&q[2]);
-                if (s[2][2] != s[3][6])
-                {
-                    d *= -1;
-                    q[3] = rotate(d, q[3]);
-                    s[3] = toString(&q[3]);
-                }
-            }
-            if (s[1][6] != s[0][2]) // 1번의 왼쪽과 0번의 오른쪽이 다르면
-            {
-                d *= -1;
-                q[0] = rotate(d, q[0]);
-                s[0] = toString(&q[0]);
-            }
-        }
-        else if (n == 2)
-        {
-            if (s[1][2] != s[2][6])
-            {
-                d *= -1;
-                q[1] = rotate(d, q[1]);
-                s[1] = toString(&q[1]);
-                if (s[0][3] != s[1][6])
-                {
-                    d *= -1;
-                    q[0] = rotate(d, q[0]);
-                    s[0] = toString(&q[0]);
-                }
-            }
-            if (s[3][6] != s[2][2])
-            {
-                d *= -1;
-                q[3] = rotate(d, q[3]);
-                s[3] = toString(&q[3]);
-            }
-        }
-        else
-        {
-            if (s[3][6] != s[2][2])
-            {
-                d *= -1;
-                q[2] = rotate(d, q[2]);
-                s[2] = toString(&q[2]);
-                if (s[2][6] != s[1][2])
-                {
-                    d *= -1;
-                    q[1] = rotate(d, q[1]);
-                    s[1] = toString(&q[1]);
-                    if (s[1][6] != s[0][2])
-                    {
-                        d *= -1;
-                        q[0] = rotate(d, q[0]);
-                        s[0] = toString(&q[0]);
-                    }
-                }
-            }
-        }
+        for (int i = 0; i < 4; i++)
+            dir[i] = 0;
     }
     int ans = 0;
 
