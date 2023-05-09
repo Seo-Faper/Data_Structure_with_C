d9df4bb28979384bfc3ee616e33e146083b06116
seo-faper <dswhdcks@gmail.com>
2023-05-09 15:44:29 +0900

    백준 동물원

--- ProblemSolve/1309.cpp
+++ ProblemSolve/1309.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 using namespace std;
+int dp[100001];
 int main()
 {
-
+    int N;
+    cin >> N;
+    dp[1] = 3;
+    dp[2] = 7;
+    for (int i = 3; i <= N; i++)
+    {
+        dp[i] = (dp[i - 2] + dp[i - 1] * 2) % 9901;
+    }
+    cout << dp[N] << endl;
     return 0;
 }
