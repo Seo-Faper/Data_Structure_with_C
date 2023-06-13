c8c0e187e3318cfe1a15d63a61b20827105f6f09
seo-faper <dswhdcks@gmail.com>
2023-06-13 15:55:19 +0900

    ㅎㅎ

---
+++ ProblemSolve/25634.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int N;
+int arr[200001];
+
+int main()
+{
+    cin >> N;
+    for (int i = 0; i < N; i++)
+    {
+        cin >> arr[i];
+    }
+    for (int i = 0; i < N; i++)
+    {
+        int flag;
+        cin >> flag;
+    }
+    return 0;
+}
