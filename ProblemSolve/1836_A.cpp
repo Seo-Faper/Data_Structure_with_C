a6562bf3a6562fee02f087c95dae6c857ff42970
seo-faper <dswhdcks@gmail.com>
2023-06-19 00:31:25 +0900

    코포 A 번

---
+++ ProblemSolve/1836_A.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int N;
+int arr[101];
+int main()
+{
+    cin >> N;
+    while (N--)
+    {
+        int p;
+        cin >> p;
+        int m = 0;
+        for (int i = 0; i < p; i++)
+        {
+            int a;
+            cin >> a;
+            m = max(m, a);
+            arr[a]++;
+        }
+        bool flag = true;
+        for (int i = 1; i <= m; i++)
+        {
+            if (arr[i - 1] < arr[i])
+                flag = false;
+        }
+        if (flag)
+        {
+            cout << "YES"
+                 << "\n";
+        }
+        else
+        {
+            cout << "NO"
+                 << "\n";
+        }
+        for (int i = 0; i < 100; i++)
+            arr[i] = 0;
+    }
+    return 0;
+}
