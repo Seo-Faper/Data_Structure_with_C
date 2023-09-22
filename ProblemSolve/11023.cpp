24f1b59696bfa48fe040e042eff09efd88fb9674
seo-faper <dswhdcks@gmail.com>
2023-09-23 02:15:35 +0900

    백준 한줄입룍

---
+++ ProblemSolve/11023.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int ans = 0;
+    string s;
+    string num = "";
+    getline(cin, s);
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (s[i] != ' ')
+            num += s[i];
+        else
+        {
+            ans += stoi(num);
+            num = "";
+        }
+    }
+    ans += stoi(num);
+    cout << ans << endl;
+    return 0;
+}
